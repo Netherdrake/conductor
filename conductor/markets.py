2ffae55780a0c715be23b678b0ab5127c3373d48
furion <_@furion.me>
2018-01-11 20:31:14 +0100

    Markets: Ignore invalid response from exchanges

--- conductor/markets.py
+++ conductor/markets.py
@@ -1,11 +1,12 @@
 import time
+from contextlib import suppress
 from decimal import Decimal
 from operator import mul
 from pprint import pprint
 from statistics import mean
 
 import requests
-from funcy.flow import silent
+from funcy import silent
 from steem import Steem
 from steem.amount import Amount
 
@@ -23,22 +24,38 @@ class Tickers(object):
         ]
         responses = list(silent(requests.get)(u, timeout=30) for u in urls)
 
-        for r in [x for x in responses if hasattr(x, "status_code") and x.status_code == 200 and x.json()]:
+        for r in [x for x in responses
+                  if hasattr(x, "status_code") and x.status_code == 200 and x.json()]:
             if "bitfinex" in r.url:
-                data = r.json()
-                prices['bitfinex'] = {'price': float(data['last_price']), 'volume': float(data['volume'])}
+                with suppress(KeyError):
+                    data = r.json()
+                    prices['bitfinex'] = {
+                        'price': float(data['last_price']),
+                        'volume': float(data['volume'])}
             elif "kraken" in r.url:
-                data = r.json()['result']['XXBTZUSD']['p']
-                prices['kraken'] = {'price': float(data[0]), 'volume': float(data[1])}
+                with suppress(KeyError):
+                    data = r.json()['result']['XXBTZUSD']['p']
+                    prices['kraken'] = {
+                        'price': float(data[0]),
+                        'volume': float(data[1])}
             elif "okcoin" in r.url:
-                data = r.json()["ticker"]
-                prices['okcoin'] = {'price': float(data['last']), 'volume': float(data['vol'])}
+                with suppress(KeyError):
+                    data = r.json()["ticker"]
+                    prices['okcoin'] = {
+                        'price': float(data['last']),
+                        'volume': float(data['vol'])}
             elif "bitstamp" in r.url:
-                data = r.json()
-                prices['bitstamp'] = {'price': float(data['last']), 'volume': float(data['volume'])}
+                with suppress(KeyError):
+                    data = r.json()
+                    prices['bitstamp'] = {
+                        'price': float(data['last']),
+                        'volume': float(data['volume'])}
             elif "btce" in r.url:
-                data = r.json()["ticker"]
-                prices['btce'] = {'price': float(data['avg']), 'volume': float(data['vol_cur'])}
+                with suppress(KeyError):
+                    data = r.json()["ticker"]
+                    prices['btce'] = {
+                        'price': float(data['avg']),
+                        'volume': float(data['vol_cur'])}
 
         if verbose:
             pprint(prices)
@@ -60,14 +77,19 @@ class Tickers(object):
         ]
         responses = list(silent(requests.get)(u, timeout=30) for u in urls)
 
-        for r in [x for x in responses if hasattr(x, "status_code") and x.status_code == 200 and x.json()]:
+        for r in [x for x in responses
+                  if hasattr(x, "status_code") and x.status_code == 200 and x.json()]:
             if "poloniex" in r.url:
-                data = r.json()["BTC_STEEM"]
-                prices['poloniex'] = {'price': float(data['last']), 'volume': float(data['baseVolume'])}
+                with suppress(KeyError):
+                    data = r.json()["BTC_STEEM"]
+                    prices['poloniex'] = {
+                        'price': float(data['last']),
+                        'volume': float(data['baseVolume'])}
             elif "bittrex" in r.url:
-                data = r.json()["result"][0]
-                price = (data['Bid'] + data['Ask']) / 2
-                prices['bittrex'] = {'price': price, 'volume': data['BaseVolume']}
+                with suppress(KeyError):
+                    data = r.json()["result"][0]
+                    price = (data['Bid'] + data['Ask']) / 2
+                    prices['bittrex'] = {'price': price, 'volume': data['BaseVolume']}
 
         if len(prices) == 0:
             raise RuntimeError("Obtaining STEEM/BTC prices has failed from all sources.")
@@ -85,18 +107,25 @@ class Tickers(object):
         ]
         responses = list(silent(requests.get)(u, timeout=30) for u in urls)
 
-        for r in [x for x in responses if hasattr(x, "status_code") and x.status_code == 200 and x.json()]:
+        for r in [x for x in responses
+                  if hasattr(x, "status_code") and x.status_code == 200 and x.json()]:
             if "poloniex" in r.url:
-                data = r.json()["BTC_SBD"]
-                if verbose:
-                    print("Spread on Poloniex is %.2f%%" % Tickers.calc_spread(data['highestBid'], data['lowestAsk']))
-                prices['poloniex'] = {'price': float(data['last']), 'volume': float(data['baseVolume'])}
+                with suppress(KeyError):
+                    data = r.json()["BTC_SBD"]
+                    if verbose:
+                        print("Spread on Poloniex is %.2f%%" %
+                              Tickers.calc_spread(data['highestBid'], data['lowestAsk']))
+                    prices['poloniex'] = {
+                        'price': float(data['last']),
+                        'volume': float(data['baseVolume'])}
             elif "bittrex" in r.url:
-                data = r.json()["result"][0]
-                if verbose:
-                    print("Spread on Bittrex is %.2f%%" % Tickers.calc_spread(data['Bid'] + data['Ask']))
-                price = (data['Bid'] + data['Ask']) / 2
-                prices['bittrex'] = {'price': price, 'volume': data['BaseVolume']}
+                with suppress(KeyError):
+                    data = r.json()["result"][0]
+                    if verbose:
+                        print("Spread on Bittrex is %.2f%%" %
+                              Tickers.calc_spread(data['Bid'], data['Ask']))
+                    price = (data['Bid'] + data['Ask']) / 2
+                    prices['bittrex'] = {'price': price, 'volume': data['BaseVolume']}
 
         if len(prices) == 0:
             raise RuntimeError("Obtaining SBD/BTC prices has failed from all sources.")
@@ -161,4 +190,5 @@ class Markets(Tickers):
 
     def avg_witness_price(self, take=10):
         price_history = self.steem.rpc.get_feed_history()['price_history']
-        return mean([Amount(x['base']).amount * Amount(x['quote']).amount for x in price_history[-take:]])
+        return mean([Amount(x['base']).amount * Amount(x['quote']).amount
+                     for x in price_history[-take:]])
